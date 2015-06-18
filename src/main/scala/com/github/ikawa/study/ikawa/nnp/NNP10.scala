082f097004b8ca715eb3c41df44e54790ce92a10
hiroyuki.ikawa <hiroyuki.ikawa@bizreach.co.jp>
2015-06-18 09:48:53 +0900

    nnp1 to 5 tailrec

--- src/main/scala/com/github/ikawa/study/ikawa/nnp/NNP10.scala
+++ src/main/scala/com/github/ikawa/study/ikawa/nnp/NNP10.scala
@@ -1,32 +1,65 @@
 package com.github.ikawa.study.ikawa.nnp
 
+import scala.annotation.tailrec
+
 /**
  * Created by hiroyuki.ikawa on 2015/06/12.
  */
 trait NNP10 {
   // 1
-  def last(list: List[Int]): Int = {
-    list.last
+  @tailrec
+  final def last(list: List[Int]): Int = {
+    //list.last
+    list match {
+      case head::Nil => head
+      case head::tail => last(tail)
+      case _ => throw new Exception
+    }
   }
 
   // 2
-  def penultimate(list: List[Int]): Int = {
-    list.init.last
+  @tailrec
+  final def penultimate(list: List[Int]): Int = {
+    //list.init.last
+    list match {
+      case head::tail::Nil => head
+      case head::tail => penultimate(tail)
+      case _ => throw new Exception
+    }
   }
 
   // 3
   def nth(n: Int, list: List[Int]): Int = {
-    list(n)
+    //list(n)
+    @tailrec
+    def ini(n: Int, l: List[Int]): List[Int] = {
+      if (n == l.size - 1) l else ini(n, l.init)
+    }
+    ini(n, list).last
   }
 
   // 4
   def length(list: List[Int]): Int = {
-    list.size
+    //list.size
+    @tailrec
+    def cnt(c: Int, l: List[Int]): Int = {
+      l match {
+        case head::Nil => c
+        case head::tail => cnt(c + 1, tail)
+        case _ => throw new Exception
+      }
+    }
+    cnt(1, list)
   }
 
   // 5
   def reverse(list: List[Int]): List[Int] = {
-    list.reverse
+    //list.reverse
+    list match {
+      case head::Nil => println("head:"+head);List(head)
+      case head::tail => reverse(tail):::head::Nil
+      case _ => throw new Exception
+    }
   }
 
   // 6
